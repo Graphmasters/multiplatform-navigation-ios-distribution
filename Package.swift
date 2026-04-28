5c536cbcb5503b78104bebe44c58d34a3eff4f2e
gm-machine <username@users.noreply.github.com>
2026-04-28 15:27:17 +0000

    :bookmark: 2.1.36 [ci skip]

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
       targets: [
           .binaryTarget(
               name: "MultiplatformNavigation",
-              url: "https://gmartifacts.jfrog.io/artifactory/mobile-libs-release-local/net/graphmasters/multiplatform/multiplatform-navigation-xcframework/2.1.35/MultiplatformNavigation.xcframework.zip",
-              checksum: "0c640389af510302876ebab87595e23574862f1f160e4e090bf6a6cb8033c8d1"
+              url: "https://gmartifacts.jfrog.io/artifactory/mobile-libs-release-local/net/graphmasters/multiplatform/multiplatform-navigation-xcframework/2.1.36/MultiplatformNavigation.xcframework.zip",
+              checksum: "7e6468fbdb53faf8d3a895b16f559764aeca79ca56f98bd1654d618373ba72a9"
           )
       ]
 )
